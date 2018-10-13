ca545a4e78709fef8ba580313c79b7334ae926c3
daria marczak <marczak.ria@gmail.com>
2018-10-13 15:51:45 +0200

    Added specifications for heroku deployment

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
-}); // Route handler in express
+});
 
-app.listen(5000);
+// Whenever heroku runs our app, it has the ability to inject environment variables. Env variables are set in the underline runtime in Node. Heroku has the ability of pass us runtime configuration. It says: look at the underline environment and verify which port we should use.
+// However in the development environment, this can be undefined.
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
