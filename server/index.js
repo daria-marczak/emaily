8fe57b83858c1e888ebd905c6253826c00a873d6
daria marczak <marczak.ria@gmail.com>
2018-10-14 18:47:38 +0200

    Server code refactoring

--- server/index.js
+++ server/index.js
@@ -1,33 +1,11 @@
 const express = require('express'); // CommonJS modules. The "import" is ES2015 modules syntax
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-const keys = require('./config/keys');
+const authRoutes = require("./routes/authRoutes");
+const passportConfig = require("./services/passport");
 
 // We might have several express applications in one app
 const app = express();
-
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.googleClientID,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback',
-		},
-		accessToken => {
-			console.log(accessToken);
-		}
-	)
-);
-
-app.get(
-	'/auth/google', // The user that comes in on the route needs to be authenticated with passport using the Google Strategy
-	passport.authenticate('google', {
-		scope: ['profile', 'email'], // Scope specifies to Google what access we want to have. We're asking Google to give us access to profile and email information
-	})
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+authRoutes(app);
 
 // Whenever heroku runs our app, it has the ability to inject environment variables. Env variables are set in the underline runtime in Node. Heroku has the ability of pass us runtime configuration. It says: look at the underline environment and verify which port we should use.
 // However in the development environment, this can be undefined.
---
+++ server/routes/authRoutes.js
@@ -0,0 +1,14 @@
+const passport = require('passport');
+
+module.exports = () => {
+  app.get(
+    '/auth/google', // The user that comes in on the route needs to be authenticated with passport using the Google Strategy
+    passport.authenticate('google', {
+      scope: ['profile', 'email'], // Scope specifies to Google what access we want to have. We're asking Google to give us access to profile and email information
+    })
+  );
+
+  app.get("/auth/google/callback", passport.authenticate("google"));
+}
+
+// We are exporting a function from here so that it has index's app instance access
