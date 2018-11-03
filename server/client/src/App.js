9a2095f3d54c2e5d4f29505b3d914851278e3cfe
daria marczak <marczak.ria@gmail.com>
2018-11-03 18:25:14 +0100

    Added proxy

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -6,6 +6,10 @@ class App extends Component {
     return (
       <div className="App">
         <h2>Hey there</h2>
+        <a href="/auth/google">Sign in with Google</a>
+        <a href="/auth/github">Sign in with Github</a>
+
+        {/* Localhost will obviously not work in a production environment */}
       </div>
     );
   }
